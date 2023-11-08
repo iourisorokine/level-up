5d7806b5aa82c663d9ed42c38101db55ec68795b
Iouri Sorokine <isorokine@gmail.com>
2023-11-08 08:56:21 +0100

    feat: levels

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,25 +11,27 @@ import { squareSymbols2 as sqSmb, SquareValue } from "./assets/squareSymbols";
 const levelReachingScores = {
   1: 20,
   2: 50,
-  3: 120,
-  4: 250,
-  5: 500,
-  6: 750,
-  7: 1000,
-  8: 1500,
-  9: 2000,
+  3: 100,
+  4: 180,
+  5: 250,
+  6: 350,
+  7: 500,
+  8: 750,
+  9: 1000,
+  10: 2000,
 };
 
 const pointsToNextLevel = {
   1: 20,
-  2: 30,
-  3: 70,
-  4: 130,
-  5: 250,
-  6: 250,
-  7: 250,
-  8: 500,
-  9: 1000,
+  2: levelReachingScores[2] - levelReachingScores[1],
+  3: levelReachingScores[3] - levelReachingScores[2],
+  4: levelReachingScores[4] - levelReachingScores[3],
+  5: levelReachingScores[5] - levelReachingScores[4],
+  6: levelReachingScores[6] - levelReachingScores[5],
+  7: levelReachingScores[7] - levelReachingScores[6],
+  8: levelReachingScores[8] - levelReachingScores[7],
+  9: levelReachingScores[9] - levelReachingScores[8],
+  10: levelReachingScores[10] - levelReachingScores[9],
 };
 
 function App() {
